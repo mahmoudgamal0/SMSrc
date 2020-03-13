6601d962472c0a0ec955d608b751d710ad7086b6
Abdelrahman Ibrahim <abdelrahman.ibrahim.009@gmail.com>
2020-03-13 16:41:44 +0200

    finished UI

--- app/src/main/java/com/example/smsrc/login/presenter/LoginPresenter.java
+++ app/src/main/java/com/example/smsrc/login/presenter/LoginPresenter.java
@@ -1,4 +1,11 @@
 package com.example.smsrc.login.presenter;
 
 public class LoginPresenter {
+
+    public boolean loginUser(String username, String password) {
+
+        return true;
+    }
+
+
 }
--- app/src/main/java/com/example/smsrc/signin/presenter/SigninPresenter.java
+++ app/src/main/java/com/example/smsrc/signin/presenter/SigninPresenter.java
@@ -1,4 +1,16 @@
 package com.example.smsrc.signin.presenter;
 
 public class SigninPresenter {
+
+
+    public boolean SignUpUser(String username, String password, String confirmPassword) {
+
+
+
+
+        //TODO
+        return true;
+    }
+
+
 }
