c8e6a8034193b8152a54d2144a0e696fe87d02f1
Muhammed Kamal <muhammedkamal97@githup.com>
2020-04-01 18:04:31 +0200

    hot fix.

--- app/src/main/java/com/example/smsrc/commands/model/CommandFactory.java
+++ app/src/main/java/com/example/smsrc/commands/model/CommandFactory.java
@@ -4,14 +4,13 @@ import android.content.Context;
 
 public class CommandFactory {
     public static Command getCommand(String commandName, Context context){
-        if(commandName.equals("ModeSwitch")){
+        if(commandName.equals(CommandsContract.MODE_SWITCH)){
             return new ModeSwitchCommand();
-        }
-        else if(commandName.equals("PlaySound")){
+        } else if(commandName.equals(CommandsContract.PLAY_SOUND)){
             return new PlaySoundCommand(context);
-        }else if(commandName.equals("ChangePinCode")){
+        } else if(commandName.equals(CommandsContract.CHANGE_PIN_CODE)){
             return new ChangePinCodeCommand();
-        }else{
+        } else{
             return null;
         }
     }
