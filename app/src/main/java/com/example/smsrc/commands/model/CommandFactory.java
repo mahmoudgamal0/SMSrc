7f7c3947a8bd139c3b1c1c09aa38508c416cb4ae
Muhammed Kamal <muhammedkamal97@githup.com>
2020-04-01 13:40:31 +0200

    add skeleton of Commands objects.

--- app/src/main/java/com/example/smsrc/commands/model/CommandFactory.java
+++ app/src/main/java/com/example/smsrc/commands/model/CommandFactory.java
@@ -5,7 +5,11 @@ public class CommandFactory {
         if(commandName.equals("ModeSwitch")){
             return new ModeSwitchCommand();
         }
-        else{
+        else if(commandName.equals("PlaySound")){
+            return new PlaySoundCommand();
+        }else if(commandName.equals("ChangePinCode")){
+            return new ChangePinCodeCommand();
+        }else{
             return null;
         }
     }
---
+++ app/src/main/java/com/example/smsrc/commands/model/PlaySoundCommand.java
@@ -0,0 +1,8 @@
+package com.example.smsrc.commands.model;
+
+public class PlaySoundCommand implements Command {
+    @Override
+    public void execute(String[] args) {
+
+    }
+}
