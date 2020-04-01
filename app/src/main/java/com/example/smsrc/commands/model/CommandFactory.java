42ab40297d03e94a3348fc0213ad00c5752c58a4
Muhammed Kamal <muhammedkamal97@githup.com>
2020-04-01 14:38:12 +0200

    implement-playSoundCommand.

--- app/src/main/java/com/example/smsrc/commands/model/CommandFactory.java
+++ app/src/main/java/com/example/smsrc/commands/model/CommandFactory.java
@@ -1,12 +1,14 @@
 package com.example.smsrc.commands.model;
 
+import android.content.Context;
+
 public class CommandFactory {
-    public static Command getCommand(String commandName){
+    public static Command getCommand(String commandName, Context context){
         if(commandName.equals("ModeSwitch")){
             return new ModeSwitchCommand();
         }
         else if(commandName.equals("PlaySound")){
-            return new PlaySoundCommand();
+            return new PlaySoundCommand(context);
         }else if(commandName.equals("ChangePinCode")){
             return new ChangePinCodeCommand();
         }else{
