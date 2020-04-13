3289b64c9ccf8468c58139168bb9e65df1ef88e6
Muhammed Kamal <muhammedkamal97@githup.com>
2020-04-13 23:28:07 +0200

    disable don't disturb mode

--- app/src/main/java/com/example/smsrc/commands/model/ModeSwitchCommand.java
+++ app/src/main/java/com/example/smsrc/commands/model/ModeSwitchCommand.java
@@ -1,7 +1,9 @@
 package com.example.smsrc.commands.model;
 
+import android.app.NotificationManager;
 import android.content.Context;
 import android.media.AudioManager;
+import android.os.Build;
 
 public class ModeSwitchCommand implements Command {
 
@@ -12,6 +14,11 @@ public class ModeSwitchCommand implements Command {
     }
     @Override
     public void execute(String[] args) {
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
+        }
         AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
     }
