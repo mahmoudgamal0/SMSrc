6904427ba5f8815a4ef2e18fdb5e0b6e47fa3499
Mahmoud Gamal <mahmoud.gamal.15497@gmail.com>
2020-04-25 14:46:59 +0200

    implemented all commands

--- app/src/main/java/com/example/smsrc/commands/model/LockPhoneCommand.java
+++ app/src/main/java/com/example/smsrc/commands/model/LockPhoneCommand.java
@@ -4,17 +4,17 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 
+import com.example.smsrc.cache.CacheManager;
 import com.example.smsrc.commands.interfaces.Command;
 import com.example.smsrc.deviceAdmin.DeviceAdminActivity;
-
-import androidx.core.view.KeyEventDispatcher;
 
 public class LockPhoneCommand implements Command {
 
     private DevicePolicyManager dpm;
     private ComponentName policyAdmin;
-
+    private Context context;
     public LockPhoneCommand(Context context) {
+        this.context = context;
         this.dpm = (DevicePolicyManager)context.getSystemService(Context.DEVICE_POLICY_SERVICE);
         this.policyAdmin = new ComponentName(context, DeviceAdminActivity.DeviceAdminActivityReceiver.class);
     }
@@ -25,11 +25,11 @@ public class LockPhoneCommand implements Command {
             throw new RuntimeException("Cannot perform this command since device admin is not activated");
         }
 
-        // TODO get pin
+        String pin = new CacheManager(context).getCachedPin();
+        if(pin != null){
+            dpm.resetPassword(pin, DevicePolicyManager.RESET_PASSWORD_REQUIRE_ENTRY);
+        }
 
-        String pin = "0000";
-        dpm.setPasswordExpirationTimeout(policyAdmin, 1);
-        dpm.resetPassword(pin, DevicePolicyManager.RESET_PASSWORD_REQUIRE_ENTRY);
         dpm.lockNow();
     }
 }
--- app/src/main/java/com/example/smsrc/commands/model/WipeDataCommand.java
+++ app/src/main/java/com/example/smsrc/commands/model/WipeDataCommand.java
@@ -3,9 +3,11 @@ package com.example.smsrc.commands.model;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.os.Build;
 
 import com.example.smsrc.commands.interfaces.Command;
 import com.example.smsrc.deviceAdmin.DeviceAdminActivity;
+import androidx.annotation.RequiresApi;
 
 public class WipeDataCommand implements Command {
 
@@ -17,12 +19,15 @@ public class WipeDataCommand implements Command {
         this.policyAdmin = new ComponentName(context, DeviceAdminActivity.DeviceAdminActivityReceiver.class);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
     @Override
     public void execute(String[] args) {
         if (!dpm.isAdminActive(policyAdmin)) {
             throw new RuntimeException("Cannot perform this command since device admin is not activated");
         }
 
+        dpm.wipeData(DevicePolicyManager.WIPE_SILENTLY);
+        dpm.wipeData(DevicePolicyManager.WIPE_EXTERNAL_STORAGE);
         dpm.wipeData(0);
     }
 }
