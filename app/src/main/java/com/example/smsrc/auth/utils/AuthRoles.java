de7d315150181ead92e6dffbcec4e3991bc7371e
Muhammed Kamal <muhammedkamal97@githup.com>
2020-05-01 22:39:26 +0200

    delete unused class.

--- app/src/main/java/com/example/smsrc/auth/utils/AuthRoles.java
+++ app/src/main/java/com/example/smsrc/auth/utils/AuthRoles.java
@@ -1,5 +1,7 @@
 package com.example.smsrc.auth.utils;
 
+import com.example.smsrc.commands.interfaces.CommandsContract;
+
 public class AuthRoles {
     public static String ADD = "add";
     public static String EDIT = "edit";
@@ -12,4 +14,15 @@ public class AuthRoles {
     public static String[] getRoles(){
         return new String[]{OWNER, LEVEL_ONE_GUEST, LEVEL_TWO_GUEST};
     }
+
+    public static String[] LEVEL_TWO_GUEST_COMMANDS = {
+            CommandsContract.PLAY_SOUND,
+            CommandsContract.IS_ALIVE
+    };
+
+    public static String[] LEVEL_ONE_GUEST_COMMANDS = {
+            CommandsContract.MODE_SWITCH,
+            CommandsContract.PLAY_SOUND,
+            CommandsContract.IS_ALIVE
+    };
 }
